0a20af42344d43b495fac79ab00b2a9c5618371e
wyrdereodyssey <wyrdereodyssey@gmail.com>
2025-09-03 17:37:30 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error! Division by zero.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        sc.close();
+    
+    }
+}
